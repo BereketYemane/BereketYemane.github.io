2af4bac6349bd80ec804aedc7cb0b0bbed30d82f
BereketYemane <87670119+BereketYemane@users.noreply.github.com>
2021-07-29 21:15:12 -0500

    commit

---
+++ W2D4/code.js
@@ -0,0 +1,48 @@
+//Number 1
+String.prototype.filter = function(arr){
+    var rgx;
+    if(arr instanceof Array){
+        rgx = new RegExp(arr.join("|"),"gi");
+    }
+    else{
+        rgx=new RegExp(arr,"gi");
+    }
+    return (this.replaceAll(rgx,''));
+};
+console.log("This house is not nice!".filter('not'));
+
+//Number 2
+Array.prototype.bubbleSort= function(){
+    let len = this.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (this[j] > this[j + 1]) {
+                let tmp = this[j];
+                this[j] = this[j + 1];
+                this[j + 1] = tmp;
+            }
+        }
+    }
+    return this;
+};
+console.log([6,4,0, 3,-2,1].bubbleSort());
+
+//Number 3
+var Person = function(){};
+Person.prototype.initialize = function(name,age){
+    this.name=name;
+    this.age=age;
+}
+Person.prototype.describe = function(){
+    return this.name+", "+this.age+", years old";
+}
+
+var Teacher = function(){};
+Teacher.prototype= new Person();
+Teacher.prototype.teach = function(subject){
+    console.log(this.name+" is now teaching "+subject);
+}
+
+var teacher1 = new Teacher();
+teacher1.initialize("Michael",35);
+teacher1.teach("WAP");
---
+++ W2D4/tests.js
@@ -0,0 +1,42 @@
+//1. Filtering banned words
+const bannedWords =['not','dirty','bad'];
+const bannedWords1='not';
+describe("Filter",function(){
+    let expected="The university is  good and the housing is .Overall, it is ";
+    let expected1="This house is  nice!";
+    
+    it(`The result after filtering the words [${bannedWords1}] from the string is ... ${expected1}`,function(){
+        assert.equal("This house is not nice!".filter(bannedWords1),expected1);
+    });
+
+    it(`The result after filtering the words [${bannedWords}] from the string is ... ${expected}`,function(){
+        assert.equal("The university is not good and the housing is dirty.Overall, it is bad".filter(bannedWords),expected);
+    });
+});
+
+
+
+//2 bubble sort .... sorting elements of an array
+
+const arr=[6,4,0, 3,-2,1];
+const arr1=[100,30,-34,1000,-11,5]
+describe("bubbleSort", function(){
+    let expected=[ -2, 0, 1, 3, 4, 6 ];
+    let expected1=[-34,-11,5,30,100,1000];
+    it(`The result of sorting the items in [${arr}] is ....[${expected}]`,function(){
+        assert.deepEqual(arr.bubbleSort(),expected);
+    });
+    it(`The result of sorting the items in [${arr1}] is ....[${expected1}]`,function(){
+        assert.deepEqual(arr1.bubbleSort(),expected1);
+    });
+});
+
+//3 testing objects created
+
+// describe("Teacher",function(){
+//     let expected = "Michael is now teaching WAP";
+
+//     it(`The result of calling the teach method of teacher is .... ${expected}`, function(){
+//         assert.equal(output,expected);
+//     });
+// });
