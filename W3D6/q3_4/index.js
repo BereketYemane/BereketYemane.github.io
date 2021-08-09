a8c3acd35f442e98ca731354f05bffc2817e3d08
BereketYemane <87670119+BereketYemane@users.noreply.github.com>
2021-08-09 01:33:59 -0500

    Update index.js

--- W3D6/q3_4/index.js
+++ W3D6/q3_4/index.js
@@ -8,13 +8,13 @@ app.set('views', path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: false }));
 
-var PRODUCTS = [
+const PRODUCTS = [
     { id: 1, name: 'Sugar', description: 'its so sweet', price: 10 },
     { id: 2, name: 'Salt', description: 'you might find it sour ', price:20 },
     { id: 3, name: 'Mango', description: 'its awesome', price: 25 }
 ]
 
-var CART=[];
+const CART=[];
 var prodIndex;
 app.get('/product/:id', (req, res) => {
      prodIndex= PRODUCTS.findIndex(pr=>pr.id===parseInt(req.params.id));
